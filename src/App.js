3aced3a04af7f3df0cb8fe0d267b287ef7a2c4d7
Adrian Valdepenas <valdepenas.adrian@gmail.com>
2020-04-03 21:36:04 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Fragment} from 'react';
+import {BrowserRouter as Router, 
+        Switch,
+        Route
+      } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home'
+import Contact from './Contact';
+import Projects from "./Projects";
+import About from "./About";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () =>{
+  return(
+    
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Switch>
+           
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/projects">
+              <Projects />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Fragment>
+      </Router>
+    
+  )
 }
 
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react"
+import {Link} from "react-router-dom"
+
+const Navbar = () => {
+    return(
+        <nav className="bg-dark navbar-dark">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact Me</Link>
+        </nav>
+    )
+}
+
+export default Navbar;
